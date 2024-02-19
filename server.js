d9559f46fe44692e6610683e4949460043341827
Kevin Devlin <azwethinkweizkd@gmail.com>
2024-02-19 15:27:19 -0700

    Updates

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-
+const dotenv = require("dotenv");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,17 +14,17 @@ app.use(express.static(path.join(__dirname, "public")));
 mongoose.set("debug", true);
 
 mongoose
-  .connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/SAndSFitnessTracker",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
-  .then(() => console.log("Connected to db successfully"))
-  .catch((e) => console.log(e));
+	.connect(
+		process.env.MONGODB_URI || "mongodb://localhost:27017/SAndSFitnessTracker",
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		}
+	)
+	.then(() => console.log("Connected to db successfully"))
+	.catch((e) => console.log(e));
 
 app.use(require("./routes"));
 
